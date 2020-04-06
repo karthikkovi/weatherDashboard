7a881aeea18bc9270c48f63053aae7d6c6f91e39
Karthik Kovi <Karthi.kovi@gmail.com>
2020-04-06 13:51:45 +0930

    Added moment and formatted date

--- updateDOM.js
+++ updateDOM.js
@@ -1,17 +1,18 @@
 class Ui {
-    
+
     constructor() {
         this.cityName = document.getElementById("city");
         this.temp = document.getElementById("temp");
         this.windSpeed = document.getElementById("windSpeed");
         this.fTemp = document.getElementById("f-temp");
         this.fHumdity = document.getElementById("f-humidity");
-        this.degree = "\u00B0"
+        this.degree = "\u00B0";
+        this.date = moment().format("MM/DD/YYYY"); ;
     }
 
     populateElements(weather) {
-        this.cityName.textContent = weather.name;
-        this.temp.textContent = `Temperature: ${Math.round(weather.main.temp - 273.15)}${this.degree} C`;
+        this.cityName.textContent = `${weather.name} ${this.date}`;
+        this.temp.textContent = `Temperature: ${Math.round(weather.main.temp - 273.15)}${this.degree}C`;
         this.windSpeed.textContent = `Wind speed: ${weather.wind.speed}`;
     }
 
