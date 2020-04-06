f40746fbe30ce651a66a1047a3ffe3e57cdbb912
Karthik Kovi <Karthi.kovi@gmail.com>
2020-04-06 10:04:13 +0930

    Updated weatherData code to a separate file and cllass

--- script.js
+++ script.js
@@ -1,19 +1,8 @@
 $(document).ready(function () {
 
-  
+  let cityName = "Adelaide";
+  const getWeatherData = new GetWeatherData(cityName);
 
-  class GetWeatherData {
-
-    constructor(cityName) {
-      this.cityName = cityName;
-      const queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&APPID=8deb631cb822b2ab88218fbddc483394";
-      $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).then(function (response) {
-        console.log(response);
-      });
-    }
-  }
+  console.log(getWeatherData)
   
 })
